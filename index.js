1e0f74158a1fe302caeb14beb5913a4732d0c135
ahmedsat <ahmedsat303@gmail.com>
2022-03-16 10:56:48 +0000

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const W = require("lambert-w-function");
+console.clear();
+let a = 1.25,
+  b = 2;
+function CalculateRequiredXP(level) {
+  return Math.round(a ** level * (level / b));
+}
+
+function CalculateLevelByXP(XP) {
+  return Math.round(W.W(b * XP * Math.log(a)) / Math.log(a));
+}
+
+module.exports = { CalculateRequiredXP, CalculateLevelByXP };
